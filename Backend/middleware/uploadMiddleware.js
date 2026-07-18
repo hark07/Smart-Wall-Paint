9c97c6b9e6565da3328fd874b1bef93086a1ab85
Hark Dhami <harkdhami08@gmail.com>
2026-07-18 20:00:44 +0545

    Fix cloudinary dependency conflict and node version

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -1,44 +1,12 @@
-import multer from "multer";
-import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.js";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() +
-        "-" +
-        Math.round(Math.random() * 1e9) +
-        path.extname(file.originalname),
-    );
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "smart-wall-paint",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png|webp/;
-
-  const extname = allowedTypes.test(
-    path.extname(file.originalname).toLowerCase(),
-  );
-
-  const mimetype = allowedTypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  }
-
-  cb(new Error("Only JPG, PNG and WEBP images are allowed"));
-};
-
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 10 * 1024 * 1024,
-  },
-});
-
-export default upload;
+export default storage;
